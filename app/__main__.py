1d804bdf6cfbf1a9e88e401339764c52540dd99b
Bakley <barclaykoin@gmail.com>
2026-02-23 09:45:08 +0300

    modified the folder

--- app/__main__.py
+++ app/__main__.py
@@ -1,9 +1,9 @@
 """
 Main entry point for running the application module directly
 """
-
 from app.cli import main
 
+# make the python script to be executable
 if __name__ == "__main__":
     main()
     
